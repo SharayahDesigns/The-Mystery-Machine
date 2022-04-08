cccf654effde4140fc5c588f98ba207494254574
Austin Kilgore <mr.kilgore@MacBook-Air.local>
2022-04-08 17:28:00 -0600

    partail about page

--- client/src/components/Pages/About.js
+++ client/src/components/Pages/About.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
+
 const About = () => {
+
   return (
     <div>
-      <h1>About</h1>
+      <h1>About the App</h1>
+      <h1>The Goal</h1>
+      <p>Solve all the puzzles, complete all the Rooms</p>
+
+      {/* <img src={'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eugenecartransport.com%2F2020%2F02%2F07%2Fmystery-machine-what-was-it%2F&psig=AOvVaw2dcODd0749ZJuoTpOaib7F&ust=1649546239533000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPiElejMhfcCFQAAAAAdAAAAABAz'}
+      alt= 'Image' ></img> */}
+      <h1>The Gang</h1>
+      <p></p>
+      <h2>Jessica</h2>
+      <p>The Brains</p>
+      <h2>Sharayah</h2>
+      <p>The Stylist</p>
+      <h2>Austin</h2>
+      <p>Is here</p>
+      
     </div>
   )
 }
 
 export default About
-
