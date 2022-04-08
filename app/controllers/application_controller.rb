e9adf3522c2005afee98f001c95f029352227327
Sharayah Hefner <rocknrayah@gmail.com>
2022-04-08 17:28:01 -0600

    profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 
 
--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import {AuthContext} from '../../providers/AuthProvider'
+import React, {useContext} from 'react'
 
 const Profile = () => {
+   let auth = useContext(AuthContext)
+  if(!auth.user) {
+  return <p> You should not be able to come here PLEASE redircet to LOGIN page</p>
+  }
+  
+  
   return (
     <div>
       <h1>Profile</h1>
+     <img className='album' src={auth.user.image}></img>
+      <p> {auth.user.name}</p>
+      <p> {auth.user.email}</p>
+      <p> points: </p>
+      <button> Edit</button>
+      
+      
+       {/* <p> {JSON.stringify(auth)}</p> */}
+      
+      
     </div>
   )
 }
