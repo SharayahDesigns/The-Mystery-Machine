920f72c47556b9b78b6a50efd39efdb0a84812f4
Sharayah Hefner <rocknrayah@gmail.com>
2022-04-09 16:24:37 -0600

    profile page

--- client/src/components/Pages/Profile.js
+++ client/src/components/Pages/Profile.js
@@ -1,34 +1,50 @@
-import {AuthContext} from '../../providers/AuthProvider'
-import React,{useContext} from 'react'
+import axios from "axios";
+import { AuthContext } from "../../providers/AuthProvider";
+import React, { useContext } from "react";
+import {useState,useEffect} from "react";
 
-
-
-
-const Profile = () => {
-   let auth = useContext(AuthContext)
-  if(!auth.user) {
-  return <p> You should not be able to come here PLEASE redircet to LOGIN page</p>
-  }
+const Profile = (props) => {
+  let auth = useContext(AuthContext);
+  const [points, setUserPoints] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getUser();
+  })
   
- 
+  
+  if (!auth.user) {
+    return (
+      <p> You should not be able to come here PLEASE redircet to LOGIN page</p>
+    );
+  }
+  const getUser = async (pointStuff) => {
+    try {
+      let res = await axios.get("/api/points");
+// res.data find the id that matches the auth.user.id  
+   
+      setUserPoints(...points, res.data)
+      setLoading(false)
+    } catch(error) {
+      setLoading(false)
+    }
+  };
+  if(loading) return <p>spinner here</p>
   
   
   return (
     <div>
       <h1>Profile</h1>
-     <img className='album' src={auth.user.image}></img>
+      <img className="album" src={auth.user.image}></img>
       <h2> {auth.user.name}</h2>
       <p> {auth.user.email}</p>
+      <p> {auth.user.id}</p>
+      
+      
+      <hr></hr>
       <p> points: </p>
-      <button> Edit</button>
-      
-     
-      
-       {/* <p> {JSON.stringify(auth)}</p> */}
-      
-      
+      <p>{JSON.stringify(points)}</p>
+      <button> Edit thing</button>
     </div>
-  )
-}
-
-export default Profile
+  );
+};
+export default Profile;
